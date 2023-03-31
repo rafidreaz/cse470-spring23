ca40c04d936e8e090100378014796c433e06e222
MD RAFID REAZ <65342116+rafidreaz@users.noreply.github.com>
2023-04-01 01:46:58 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get("/home",[HomeController::class,"index"]);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
