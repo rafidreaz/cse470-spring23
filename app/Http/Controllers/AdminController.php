d987407e5d09a86fcca9f9719ec8b2925829394f
MD RAFID REAZ <65342116+rafidreaz@users.noreply.github.com>
2023-04-04 11:18:10 +0600

    chef management

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Food;
 use App\Models\Reservation;
+use App\Models\Foodchef;
+
 class AdminController extends Controller
 {
     public function user(){
@@ -56,9 +58,9 @@ class AdminController extends Controller
     public function upload(Request $request){
         $data= new food;
         $image= $request->image;
+
         $imagename =time().'.'.$image->getClientOriginalExtension();
         $request->image->move('foodimage',$imagename);
-
         $data->image=$imagename;
 
         $data->title=$request->title;
@@ -101,5 +103,54 @@ class AdminController extends Controller
 
         return view("admin.adminreservation",compact("data"));
     }
+    public function viewchef(){
+        $data=foodchef::all();
+        return view("admin.adminchef",compact("data"));
+    }
+    public function uploadchef(Request $request){
+        $data = new foodchef;
+        $image=$request->image;
+
+        $imagename =time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('chefimage',$imagename);
+        $data->image=$imagename;
+
+        $data->name=$request->name;
+
+        $data->speciality=$request->speciality;
+
+        $data->save();
+
+        return redirect()->back();
+    }
+
+    public function updatechef($id){
+        $data=foodchef::find($id);
+        return view("admin.updatechef",compact("data"));
+    }
+
+    public function updatefoodchef(Request $request ,$id){
+        $data=foodchef::find($id);
+        $image= $request->image;
+if($image){
+        $imagename =time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('chefimage',$imagename);
+        $data->image=$imagename;
+}
+        $data->name=$request->name;
+        $data->speciality=$request->speciality;
+
+        $data->save();
+
+        return redirect()->back();
+    }
+    public function deletechef($id){
+        $data=foodchef::find($id);
+
+        $data->delete();
+
+        return redirect()->back();
+    }
+
 
 }
